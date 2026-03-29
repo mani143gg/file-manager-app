542c7d98fff2acb128cd0cd62f51a26b11437201
mani143gg <manikandan143g@gmail.com>
2026-03-29 23:36:46 +0530

    feat: add video upload component with AWS MediaConvert integration and job queue management

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { FileUploadComponent } from './file-upload/file-upload';
 import { RouterOutlet } from '@angular/router';
+import { VideoUploadComponent } from './video-upload/video-upload';
+
 
 @Component({
   selector: 'app-root',
-  imports: [FileUploadComponent],
+  imports: [FileUploadComponent, VideoUploadComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
